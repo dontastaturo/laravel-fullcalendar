b1a1c6fb639e8740d5046a02f7601831826105a5
Ralf Siebert <ralf.siebert@protonmail.com>
2020-03-31 16:26:45 +0200

    removed crap

--- src/views/script.blade.php
+++ src/views/script.blade.php
@@ -4,7 +4,6 @@
 
 <script type="text/javascript">
     jQuery(document).ready(function () {
-        var calendar_{{$id}} = new FullCalendar.Calendar(document.getElementById('{{$id}}'), {!! $options !!}).render();calendar_{{$id}}.refetchEvents();
-        console.log('ready');
+        new FullCalendar.Calendar(document.getElementById('{{$id}}'), {!! $options !!}).render();
     });
 </script>
