94ff34964e93c4e136216f755292804591744288
Ralf Siebert <ralf.siebert@protonmail.com>
2020-03-31 16:11:45 +0200

    test

--- src/views/script.blade.php
+++ src/views/script.blade.php
@@ -4,7 +4,7 @@
 
 <script type="text/javascript">
     jQuery(document).ready(function () {
-        var calendar_{{$id}} = new FullCalendar.Calendar(document.getElementById('{{$id}}'), {!! $options !!}).render().refetchEvents();
+        var calendar_{{$id}} = new FullCalendar.Calendar(document.getElementById('{{$id}}'), {!! $options !!}).render();calendar_{{$id}}.refetchEvents();
         console.log('ready');
     });
 </script>
