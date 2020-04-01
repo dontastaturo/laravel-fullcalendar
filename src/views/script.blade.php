2819dd60828b38685f6e689bb40fe9fc66be64ad
Ralf Siebert <ralf.siebert@protonmail.com>
2020-04-01 17:38:56 +0200

    test

--- src/views/script.blade.php
+++ src/views/script.blade.php
@@ -4,9 +4,7 @@
 
 <script>
     jQuery(document).ready(function () {
-        var calEl = document.getElementById('{{$id}}');
         var calendar = new FullCalendar.Calendar(document.getElementById('{{$id}}')), {!! $options !!});
-        calEl.calendar = calendar;
         if(calendars) {
             calendars['{{$id}}'] = calendar;
         }
