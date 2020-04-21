2b29d5a0d255a692cf7deaaca76e24c3a2354c30
Ralf Siebert <ralf.siebert@protonmail.com>
2020-04-21 16:13:20 +0200

    optimized check if calendars is defined

--- src/views/script.blade.php
+++ src/views/script.blade.php
@@ -5,7 +5,7 @@
 <script>
     jQuery(document).ready(function () {
         var calendar = new FullCalendar.Calendar(document.getElementById('{{$id}}'), {!! $options !!});
-        if(calendars) {
+        if(calendars || false) {
             calendars['{{$id}}'] = calendar;
         }
         // calendar.render();
